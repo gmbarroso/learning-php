205cd32f9cbb2ccc1d4e978b560e628cad7892f8
gmbarroso <barroso.guilherme@gmail.com>
2019-11-25 23:07:41 -0300

    first commit

---
+++ variaveis/exemplo1.php
@@ -0,0 +1,10 @@
+<?php
+// OBS: sempre terminando com ;
+
+$nome = "teste"; // declarando uma variável em php
+
+// var_dump($nome); // para saber que tipo de variável é
+
+echo $nome; // exibindo o valor na tela
+
+?>
