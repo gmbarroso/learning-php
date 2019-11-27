23f25bb78c794b17e827dc47ace223f06a712769
gbarroso84 <gbarroso@bionexo.com>
2019-11-27 17:46:44 -0300

    add constantes

---
+++ 13.constantes/exemplo1.php
@@ -0,0 +1,8 @@
+<?php
+// constantes, segue o mesmo conceito de const do JS
+// variáveis que receberão valores que não mudará em nenhum momento na execução do meu código
+define("SERVIDOR", "127.0.0.1"); // uma constante é definida por uma função chamada define que passa-se o nome primeiro e depois o valor
+// é uma boa prática declará-las sempre em maiúsculo
+
+echo SERVIDOR;
+?>
